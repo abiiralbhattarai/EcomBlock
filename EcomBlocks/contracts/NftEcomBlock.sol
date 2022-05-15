d2836c958f8d1c8f7cbc0d172a08d53d49a55d0d
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-05-15 15:40:52 +0545

    package added

--- EcomBlocks/contracts/NftEcomBlock.sol
+++ EcomBlocks/contracts/NftEcomBlock.sol
@@ -6,38 +6,34 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NftEcomBlock is ERC721Enumerable, Ownable {
-  using Strings for uint256;
+    using Strings for uint256;
 
-  string public baseURI;
-  string public baseExtension = ".json";
-  uint256 public nftCost = 0.05 ether;
-  uint256 public maxnftSupply = 100;
-  bool public onlyWhitelisted = true;
-  address[] public whitelistedAddresses;
-  mapping(address => uint256) public nftAllowed;
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public nftCost = 0.05 ether;
+    uint256 public maxnftSupply = 100;
+    bool public onlyWhitelisted = true;
+    address[] public whitelistedAddresses;
+    mapping(address => uint256) public nftAllowed;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    // string memory _initBaseURI
-  )ERC721(_name, _symbol) {
-    // setBaseURI(_initBaseURI);
-  }
-
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-
-  // mint function
-  function mint(uint256 _mintAmount) public payable {
-    // uint256 supply = totalSupply();
-    // require(_mintAmount > 0, "You need to mint at least 1 NFT");
-    // require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
-    // require(isWhiteListed(msg.sender), "You are not whitelisted");
-    // uint256 MintedCount = addressMintedBalance[msg.sender];
-    // require(MintedCount + _mintAmount <= nftPerAddressLimit, "NFT allowe to mint per address exceeded");
+    constructor(string memory _name, string memory _symbol)
+        // string memory _initBaseURI
+        ERC721(_name, _symbol)
+    {
+        // setBaseURI(_initBaseURI);
     }
 
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // mint function
+    function mint(uint256 _mintAmount) public payable {
+        // uint256 supply = totalSupply();
+        // require(_mintAmount > 0, "You need to mint at least 1 NFT");
+        // require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+        // require(isWhiteListed(msg.sender), "You are not whitelisted");
+        // uint256 MintedCount = addressMintedBalance[msg.sender];
+        // require(MintedCount + _mintAmount <= nftPerAddressLimit, "NFT allowe to mint per address exceeded");
+    }
 }
-
-
